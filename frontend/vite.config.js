051f09a83a341200c2959c4a7fffcda56d35883a
Oxbee59 <agyeiosborn59@gmail.com>
2025-12-08 09:48:09 +0000

    Add redirects file for React Router on Render

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
-// https://vite.dev/config/
+// Copy _redirects file into dist
+import { viteStaticCopy } from "vite-plugin-static-copy";
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: resolve(__dirname, "public/_redirects"),
+          dest: "./"
+        }
+      ]
+    })
+  ],
+
+  build: {
+    outDir: "dist",
+  }
+});
